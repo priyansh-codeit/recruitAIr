86506ca2efdd9f9cb29cef0a15291cb2271931ce
Harshit Gautam <harshitgautam2110@gmail.com>
2026-06-30 21:42:26 +0000

    Add Cognee memory module

---
+++ backend/app/cognee/__init__.py
@@ -0,0 +1,5 @@
+from .remember import remember_resume
+from .recall import search_candidates
+from .forget import delete_all_candidates
+from .improve import improve_memory
+from .interaction import remember_interaction
